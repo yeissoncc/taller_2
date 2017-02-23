717369f205584cb59b4879c0c8dde8caaca0959d
yeisson castro <yeissoncc@gmail.com>
2017-02-23 09:47:05 -0500

    circuferencia

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+nombre del programa : teblas de multiplicar
+autor :yeissonestiven castro
+fecha: 23/ 0w2 /2017
+
+*/
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	int tabla;
+	printf("ingrese el numero de la tabla \n");
+	scanf("%d",&tabla);
+	printf("%d x 1 = %d \n",tabla,tabla*1);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*2);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*3);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*4);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*5);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*6);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*7);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*8);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*9);
+	
+	printf("%d x 1 = %d \n",tabla,tabla*10);
+	return 0;
+}
